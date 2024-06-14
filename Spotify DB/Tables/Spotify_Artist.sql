9a4246f1ddc5fe0f69b216bede02777ce9d48a42
Aakarsh Singh <aakarshsingh2406@gmail.com>
2024-06-14 06:40:51 +0000

    Enhancement_Remove_Quotes_from_table_name

--- Spotify DB/Tables/Spotify_Artist.sql
+++ Spotify DB/Tables/Spotify_Artist.sql
@@ -1,4 +1,4 @@
-CREATE OR REPLACE TABLE "Spotify_Artist"(
+CREATE OR REPLACE TABLE Spotify_Artist(
     "Artist_Id" INT NOT NULL,
     "Artist_Name" VARCHAR(255) collate 'en-ci' NOT NULL,
     "External_Url" VARCHAR(255) collate 'en-ci' NOT NULL
